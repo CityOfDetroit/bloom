b695d4a85293ed5f892b1dadaa7a4c2d459b6517
elenm <16543000+elenm@users.noreply.github.com>
2021-06-10 22:47:32 -0400

    Add preliminary Detroit site styles.

--- sites/public/tailwind.config.js
+++ sites/public/tailwind.config.js
@@ -8,4 +8,14 @@ const bloomTheme = cloneDeep(require("@bloom-housing/ui-components/tailwind.conf
 // For example:
 // bloomTheme.theme.colors.white = "#f0f0e9"
 
+bloomTheme.theme.colors["primary-darker"] = "#004445"
+bloomTheme.theme.colors["primary-dark"] = "#004445"
+bloomTheme.theme.colors.primary = "#279989"
+bloomTheme.theme.colors["primary-light"] = "#F2F2F2"
+bloomTheme.theme.colors["primary-lighter"] = "#F2F2F2"
+bloomTheme.theme.colors["gray-800"] = "#18252A"
+bloomTheme.theme.colors["gray-950"] = "#000000"
+bloomTheme.theme.colors.warn = "#feb70d"
+bloomTheme.theme.colors["accent-cool"] = "#279989"
+
 module.exports = bloomTheme
