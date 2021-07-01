6b6dd3b89418c17d6042dc1b0c65c9069d9c033b
Abbie Farr <afarr@google.com>
2021-07-01 11:47:46 -0400

    Fix issues in public and partner sites

--- sites/public/pages/index.tsx
+++ sites/public/pages/index.tsx
@@ -82,7 +82,7 @@ export async function getStaticProps() {
   let listings = []
   try {
     const response = await axios.get(process.env.listingServiceUrl)
-    listings = response.data.items
+    listings = response.data.items ? response.data.items : null
   } catch (error) {
     console.error(error)
   }
