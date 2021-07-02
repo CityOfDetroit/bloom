6d9d7492fc98cac3e1baac703f841d7a39163a8c
Will Lin <wrl@google.com>
2021-07-02 16:17:00 -0400

    Fix the metadata from the axios response.
    I had the incorrect type on the axios response, which led to the
    metadata being undefined, so no pages were being generated.

--- sites/public/pages/listings/page/[page].tsx
+++ sites/public/pages/listings/page/[page].tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 import { ListingsList, PageHeader, t } from "@bloom-housing/ui-components"
 import { Listing, PaginatedListings } from "@bloom-housing/backend-core/types"
 import Layout from "../../../layouts/application"
@@ -38,15 +38,28 @@ export default function ListingsPage(props: ListingsProps) {
 
 export async function getStaticPaths(context: { locales: Array<string> }) {
   try {
-    const response: PaginatedListings = await axios.get(process.env.listingServiceUrl, {
-      params: { page: "1", limit: "10" },
-    })
+    const response: AxiosResponse<PaginatedListings> = await axios.get(
+      process.env.listingServiceUrl,
+      {
+        // TODO: Think about making this limit configurable.
+        params: { page: "1", limit: "10" },
+      }
+    )
 
+    /**
+     * We want to return an array inside paths that contains an entry for each combination
+     * of locale and page number. So we create an array that goes from 1 .. totalPages and
+     * map that with locales.
+     */
     return {
-      paths: context.locales.flatMap((locale: string) => ({
-        params: { page: response.meta.totalPages },
-        locale: locale,
-      })),
+      paths: context.locales.flatMap((locale: string) =>
+        Array.from({ length: response.data.meta.totalPages }, (_, index) => index + 1).map(
+          (currentPage: number) => ({
+            params: { page: currentPage.toString() },
+            locale: locale,
+          })
+        )
+      ),
       fallback: false,
     }
   } catch (e) {
@@ -62,10 +75,10 @@ export async function getStaticProps(context: { params: Record<string, string> }
     const response = await axios.get(process.env.listingServiceUrl, {
       params: { page: context.params.page, limit: "10" },
     })
-    return { props: { listings: response.data.items }, revalidate: 120 }
+    return { props: { listings: response.data.items } }
   } catch (error) {
     console.error(error)
   }
 
-  return { props: { listings: [] }, revalidate: 120 }
+  return { props: { listings: [] } }
 }
