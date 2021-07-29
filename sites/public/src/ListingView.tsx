54e8044125f906d0e56d287df9eff0c6eda5d99d
pfeeny5 <82653098+pfeeny5@users.noreply.github.com>
2021-07-29 15:35:38 -0400

    created listing updated on panel for sidebar on listings page

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -33,6 +33,7 @@ import {
   OpenHouseEvent,
   DownloadLotteryResults,
   ReferralApplication,
+  ListingUpdated,
 } from "@bloom-housing/ui-components"
 import moment from "moment"
 import { ErrorPage } from "../pages/_error"
@@ -308,6 +309,7 @@ export const ListingView = (props: ListingProps) => {
         >
           <aside className="w-full static md:absolute md:right-0 md:w-1/3 md:top-0 sm:w-2/3 md:ml-2 h-full md:border border-gray-400 bg-white">
             <div className="hidden md:block">
+              <ListingUpdated listing={listing} />
               <ApplicationStatus listing={listing} />
               <DownloadLotteryResults event={lotteryResults} />
               {openHouseEvents && <OpenHouseEvent events={openHouseEvents} />}
