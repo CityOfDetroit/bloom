0ff0a61f98bfd6a6d6e02e2370c4f06a79bb6871
Austin Valeske <avaleske@google.com>
2021-08-11 01:30:18 -0400

    fix test and migration indentation

--- backend/core/src/migration/1628631437422-AddNumBedroomsFieldToUnitType.ts
+++ backend/core/src/migration/1628631437422-AddNumBedroomsFieldToUnitType.ts
@@ -5,14 +5,14 @@ export class AddNumBedroomsFieldToUnitType1628631437422 implements MigrationInte
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`ALTER TABLE "unit_types" ADD "num_bedrooms" integer`)
-    queryRunner.query(`UPDATE unit_types SET num_bedrooms = CASE
-                                                                WHEN name = 'studio' THEN 0
-                                                                WHEN name = 'oneBdrm' THEN 1
-                                                                WHEN name = 'twoBdrm' THEN 2
-                                                                WHEN name = 'threeBdrm' THEN 3
-                                                                WHEN name = 'fourBdrm' THEN 4
-                                                                WHEN name = 'fiveBdrm' THEN 5
-                                                              END`)
+    queryRunner.query(`UPDATE "unit_types" SET "num_bedrooms" = CASE
+  WHEN "name" = 'studio' THEN 0
+  WHEN "name" = 'oneBdrm' THEN 1
+  WHEN "name" = 'twoBdrm' THEN 2
+  WHEN "name" = 'threeBdrm' THEN 3
+  WHEN "name" = 'fourBdrm' THEN 4
+  WHEN "name" = 'fiveBdrm' THEN 5
+END`)
     queryRunner.query(`ALTER TABLE "unit_types" ALTER COLUMN "num_bedrooms" SET NOT NULL`)
   }
 
