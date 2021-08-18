81da6a6635322320ba78f70146de6eb254b4f00a
Lint Action <lint-action@samuelmeuli.com>
2021-08-18 18:22:08 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1629309730346-unitsDefaultStatusAndUserRoles.ts
+++ backend/core/src/migration/1629309730346-unitsDefaultStatusAndUserRoles.ts
@@ -1,22 +1,33 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class unitsDefaultStatusAndUserRoles1629309730346 implements MigrationInterface {
-    name = 'unitsDefaultStatusAndUserRoles1629309730346'
+  name = "unitsDefaultStatusAndUserRoles1629309730346"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" SET DEFAULT 'unknown'`);
-        await queryRunner.query(`ALTER TABLE "user_roles" DROP CONSTRAINT "FK_87b8888186ca9769c960e926870"`);
-        await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "UQ_87b8888186ca9769c960e926870" UNIQUE ("user_id")`);
-        await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "user_accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" SET NOT NULL`)
+    await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" SET DEFAULT 'unknown'`)
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" DROP CONSTRAINT "FK_87b8888186ca9769c960e926870"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" ADD CONSTRAINT "UQ_87b8888186ca9769c960e926870" UNIQUE ("user_id")`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "user_accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_roles" DROP CONSTRAINT "FK_87b8888186ca9769c960e926870"`);
-        await queryRunner.query(`ALTER TABLE "user_roles" DROP CONSTRAINT "UQ_87b8888186ca9769c960e926870"`);
-        await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "user_accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" DROP NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" DROP CONSTRAINT "FK_87b8888186ca9769c960e926870"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" DROP CONSTRAINT "UQ_87b8888186ca9769c960e926870"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "user_accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    )
+    await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" DROP DEFAULT`)
+    await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" DROP NOT NULL`)
+  }
 }
