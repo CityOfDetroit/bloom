035d4c29dbf6ac5225f5abe47dd51d1cd711a8cc
Lint Action <lint-action@samuelmeuli.com>
2021-08-20 20:11:47 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1629475677933-rename-unit-summary-column.ts
+++ backend/core/src/migration/1629475677933-rename-unit-summary-column.ts
@@ -1,20 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class renameUnitSummaryColumn1629475677933 implements MigrationInterface {
-    name = 'renameUnitSummaryColumn1629475677933'
+  name = "renameUnitSummaryColumn1629475677933"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "minimum_income_min"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "minimum_income_max"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_income_min" text`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_income_max" text`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "minimum_income_min"`)
+    await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "minimum_income_max"`)
+    await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_income_min" text`)
+    await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_income_max" text`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_income_max"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_income_min"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "minimum_income_max" text`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "minimum_income_min" text`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_income_max"`)
+    await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_income_min"`)
+    await queryRunner.query(`ALTER TABLE "units_summary" ADD "minimum_income_max" text`)
+    await queryRunner.query(`ALTER TABLE "units_summary" ADD "minimum_income_min" text`)
+  }
 }
