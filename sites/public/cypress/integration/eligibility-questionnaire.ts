71975ede74d115df28947b61d6145452c7f41ea9
Anders Schneider <anderseschneider@gmail.com>
2021-08-23 13:12:47 -0400

    Remove checks for specific text

--- sites/public/cypress/integration/eligibility-questionnaire.ts
+++ sites/public/cypress/integration/eligibility-questionnaire.ts
@@ -13,13 +13,9 @@ describe("Verifying the eligibility questionnaire flow", () => {
   it("Navigates through the eligibility questionnaire flow", () => {
     cy.visit("/eligibility/welcome")
 
-    // Verify that the page welcomes us :)
-    cy.contains("Welcome")
-
     // Click the "Next" button to go to the "Bedrooms" section.
     cy.contains("Next").click()
     cy.url().should("include", "/eligibility/bedrooms")
-    cy.contains("How many bedrooms do you need?")
 
     // Find and click to indicate both 2BR and 3BR
     cy.get("#twoBdrm").click()
@@ -28,7 +24,6 @@ describe("Verifying the eligibility questionnaire flow", () => {
     // Click "Next" to go to the "Age" section
     cy.contains("Next").click()
     cy.url().should("include", "/eligibility/age")
-    cy.contains("How old are you?")
 
     // Enter "55" for the age input.
     cy.get("input").type("55")
@@ -36,7 +31,6 @@ describe("Verifying the eligibility questionnaire flow", () => {
     // Click "Next" to go to the "Disability" section
     cy.contains("Next").click()
     cy.url().should("include", "/eligibility/disability")
-    cy.contains("Do you have a disability?")
 
     // Click "no" to indicate no disability.
     cy.get("#disabilityNo").click()
@@ -44,7 +38,6 @@ describe("Verifying the eligibility questionnaire flow", () => {
     // Click "Next" to go to the "Income" section
     cy.contains("Next").click()
     cy.url().should("include", "/eligibility/income")
-    cy.contains("What is your total household annual income?")
 
     // Select "$30k to $40k"
     cy.get("select").select("30kTo40k")
@@ -52,7 +45,6 @@ describe("Verifying the eligibility questionnaire flow", () => {
     // Click "Next" to go to the "Section 8" section
     cy.contains("Next").click()
     cy.url().should("include", "/eligibility/section8")
-    cy.contains("Do you have a Section 8 voucher?")
 
     // Click "no" to indicate no Section 8 voucher.
     cy.get("#section8No").click()
