3a1434a77af84545ecb0e5e8ae374613ef86fef7
Anders Schneider <anderseschneider@gmail.com>
2021-08-24 12:25:08 -0400

    Very simple test passes, jest.config.js def can be pared down (it was copied from ui-components); blocker right now: interpreting React components in the test

---
+++ sites/public/__tests__/eligibility_questionnaire/EligibilityWelcome.test.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import EligibilityWelcome from "../../pages/eligibility/welcome"
+
+describe("<EligibilityWelcome>", () => {
+  it("please just work", () => {
+    // const { getByText } = render(<EligibilityWelcome />)
+    // expect(getByText("Welcome")).toBeTruthy()
+    console.log("HERE")
+  })
+})
---
+++ sites/public/jest.config.js
@@ -0,0 +1,35 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+process.env.TZ = "UTC"
+
+module.exports = {
+  testRegex: ["/*.test.tsx$", "/*.test.ts$"],
+  collectCoverageFrom: ["**/*.tsx", "!**/*.stories.tsx"],
+  coverageReporters: ["lcov", "text"],
+  coverageDirectory: "test-coverage",
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json",
+    },
+  },
+  rootDir: "../..",
+  roots: ["<rootDir>/sites/public"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "ts-jest",
+  },
+  setupFiles: ["dotenv/config"],
+  moduleNameMapper: {
+    "\\.(scss|css|less)$": "identity-obj-proxy",
+  },
+  testPathIgnorePatterns: ["storyshots.d.ts"],
+}
