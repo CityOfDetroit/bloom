3f7387d32e633b3a851c598d9b5220afc2acc650
pfeeny5 <82653098+pfeeny5@users.noreply.github.com>
2021-08-25 14:36:03 -0400

    revert Detroit seed data from proto

--- backend/core/src/seeds/listings/listing-detroit-10145.ts
+++ backend/core/src/seeds/listings/listing-detroit-10145.ts
@@ -145,8 +145,6 @@ export class Listing10145Seed extends ListingDefaultSeed {
       unitType: unitTypeTwoBdrm,
       totalCount: 142,
       listing: listing,
-      minimumIncomeMin: "4,000",
-      minimumIncomeMax: "10,000",
     }
     mcvUnitsSummaryToBeCreated.push(twoBdrmUnitsSummary)
 
