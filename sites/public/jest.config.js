2b3e4587e4685a9519208f02b2776987e01ff9a8
Anders Schneider <anderseschneider@gmail.com>
2021-08-25 15:19:38 -0400

    New tsconfig

--- sites/public/jest.config.js
+++ sites/public/jest.config.js
@@ -19,7 +19,7 @@ module.exports = {
   preset: "ts-jest",
   globals: {
     "ts-jest": {
-      tsConfig: "tsconfig.json",
+      tsConfig: "__tests__/tsconfig.json",
     },
   },
   rootDir: "../..",
