9ca59c461399c3fbc57340c22b7906df64a18042
Anders Schneider <anderseschneider@gmail.com>
2021-08-25 15:31:37 -0400

    Pare down jest config to only what's needed

--- sites/public/jest.config.js
+++ sites/public/jest.config.js
@@ -1,22 +1,5 @@
-/*eslint no-undef: "error"*/
-/*eslint-env node*/
-
-process.env.TZ = "UTC"
-
 module.exports = {
   testRegex: ["/*.test.tsx$", "/*.test.ts$"],
-  collectCoverageFrom: ["**/*.tsx", "!**/*.stories.tsx"],
-  coverageReporters: ["lcov", "text"],
-  coverageDirectory: "test-coverage",
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
-    },
-  },
-  preset: "ts-jest",
   globals: {
     "ts-jest": {
       tsConfig: "__tests__/tsconfig.json",
@@ -27,10 +10,8 @@ module.exports = {
   transform: {
     "^.+\\.[t|j]sx?$": "ts-jest",
   },
-  setupFiles: ["dotenv/config"],
   setupFilesAfterEnv: ["<rootDir>/sites/public/.jest/setup-tests.js"],
   moduleNameMapper: {
     "\\.(scss|css|less)$": "identity-obj-proxy",
   },
-  testPathIgnorePatterns: ["storyshots.d.ts"],
 }
