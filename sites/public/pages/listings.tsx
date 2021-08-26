4d0764de9284ba92a25c8521d50d347250896eb0
elenm <16543000+elenm@users.noreply.github.com>
2021-08-26 16:13:52 -0400

    Add senior housing filtering to the filter modal.

--- sites/public/pages/listings.tsx
+++ sites/public/pages/listings.tsx
@@ -75,6 +75,12 @@ const ListingsPage = () => {
     { value: "Foster City", label: "Foster City" },
   ]
 
+  const seniorHousingOptions: SelectOption[] = [
+    EMPTY_OPTION,
+    { value: "true", label: t("t.yes") },
+    { value: "false", label: t("t.no") },
+  ]
+
   function setQueryString(page: number, filters = filterState) {
     void router.push(`/listings?page=${page}${encodeToFrontendFilterString(filters)}`, undefined, {
       shallow: true,
@@ -170,6 +176,13 @@ const ListingsPage = () => {
               register={register}
               controlClassName="control"
               options={communityOptions}
+            />
+            <Select
+              id="seniorHousing"
+              name="seniorHousing"
+              label={t("listingFilters.seniorHousing")}
+              register={register}
+              options={seniorHousingOptions}
             />
           </div>
           <div className="text-center mt-6">
