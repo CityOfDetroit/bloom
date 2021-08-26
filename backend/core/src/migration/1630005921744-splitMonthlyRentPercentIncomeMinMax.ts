0e5245b2ce5e9ddfc1102f405f77d79dda3fd533
Lint Action <lint-action@samuelmeuli.com>
2021-08-26 19:37:08 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1630005921744-splitMonthlyRentPercentIncomeMinMax.ts
+++ backend/core/src/migration/1630005921744-splitMonthlyRentPercentIncomeMinMax.ts
@@ -1,18 +1,29 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class splitMonthlyRentPercentIncomeMinMax1630005921744 implements MigrationInterface {
-    name = 'splitMonthlyRentPercentIncomeMinMax1630005921744'
+  name = "splitMonthlyRentPercentIncomeMinMax1630005921744"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income_min" numeric(8,2)`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income_max" numeric(8,2)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income_min" numeric(8,2)`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income_max" numeric(8,2)`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income_max"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income_min"`);
-        await queryRunner.query(`ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income" numeric(8,2)`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income_max"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" DROP COLUMN "monthly_rent_as_percent_of_income_min"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "units_summary" ADD "monthly_rent_as_percent_of_income" numeric(8,2)`
+    )
+  }
 }
