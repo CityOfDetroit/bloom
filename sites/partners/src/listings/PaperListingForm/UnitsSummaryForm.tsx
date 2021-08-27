4a395ba2498a50bf355341773c0b3c478573a815
Lint Action <lint-action@samuelmeuli.com>
2021-08-27 21:04:52 +0000

    Fix code style issues with Prettier

--- sites/partners/src/listings/PaperListingForm/UnitsSummaryForm.tsx
+++ sites/partners/src/listings/PaperListingForm/UnitsSummaryForm.tsx
@@ -122,7 +122,7 @@ const UnitsSummaryForm = ({
         ...formData,
         id: undefined,
         tempId: summaries.length + 1,
-        listing: undefined
+        listing: undefined,
       })
     }
     setTempId(null)
