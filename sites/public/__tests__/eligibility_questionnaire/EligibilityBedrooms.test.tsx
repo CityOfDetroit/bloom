10d0b67084e618f8c147e223e496d89d6735e51c
Lint Action <lint-action@samuelmeuli.com>
2021-08-27 16:35:50 +0000

    Fix code style issues with Prettier

--- sites/public/__tests__/eligibility_questionnaire/EligibilityBedrooms.test.tsx
+++ sites/public/__tests__/eligibility_questionnaire/EligibilityBedrooms.test.tsx
@@ -1,4 +1,11 @@
-import { render, fireEvent, screen, waitFor, waitForElementToBeRemoved, queryByText } from "@testing-library/react"
+import {
+  render,
+  fireEvent,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+  queryByText,
+} from "@testing-library/react"
 import EligibilityBedrooms from "../../pages/eligibility/bedrooms"
 import React from "react"
 import { act } from "react-dom/test-utils"
