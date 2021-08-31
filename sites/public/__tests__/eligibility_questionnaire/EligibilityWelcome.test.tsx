62cddd198cf235b4ea811f801d93d9fa70958590
Anders Schneider <anderseschneider@gmail.com>
2021-08-31 11:48:33 -0400

    Remove unused import

--- sites/public/__tests__/eligibility_questionnaire/EligibilityWelcome.test.tsx
+++ sites/public/__tests__/eligibility_questionnaire/EligibilityWelcome.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen, waitForElementToBeRemoved } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 import EligibilityWelcome from "../../pages/eligibility/welcome"
 import React from "react"
 import { act } from "react-dom/test-utils";
