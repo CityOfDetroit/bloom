a5fa678b32116a82d183ae64acd0b3651bb76ace
Austin Valeske <avaleske@google.com>
2021-09-09 14:49:32 -0400

    first draft of replacing ListingFilterParams on the frontend

--- sites/public/lib/hooks.ts
+++ sites/public/lib/hooks.ts
@@ -9,8 +9,9 @@ import {
   openDateState,
   t,
   encodeToBackendFilterString,
+  ListingFilterState,
 } from "@bloom-housing/ui-components"
-import { Listing, ListingReviewOrder, ListingFilterParams } from "@bloom-housing/backend-core/types"
+import { Listing, ListingReviewOrder } from "@bloom-housing/backend-core/types"
 import { AppSubmissionContext } from "./AppSubmissionContext"
 import { ParsedUrlQuery } from "querystring"
 
@@ -42,7 +43,7 @@ export const useFormConductor = (stepName: string) => {
 }
 
 const listingsFetcher = function () {
-  return async (url: string, page: number, limit: number, filters: ListingFilterParams) => {
+  return async (url: string, page: number, limit: number, filters: ListingFilterState) => {
     const res = await axios.get(
       `${url}?page=${page}&limit=${limit}${encodeToBackendFilterString(filters)}`
     )
@@ -51,7 +52,7 @@ const listingsFetcher = function () {
 }
 
 // TODO: move this so it can be shared with the partner site.
-export function useListingsData(pageIndex: number, limit = 10, filters: ListingFilterParams) {
+export function useListingsData(pageIndex: number, limit = 10, filters: ListingFilterState) {
   const { data, error } = useSWR(
     [`${process.env.listingServiceUrl}`, pageIndex, limit, filters],
     listingsFetcher()
--- ui-components/src/helpers/filters.ts
+++ ui-components/src/helpers/filters.ts
@@ -1,7 +1,7 @@
 import {
   EnumListingFilterParamsComparison,
   ListingFilterKeys,
-  ListingFilterParams,
+  AvailabilityFilterEnum,
 } from "@bloom-housing/backend-core/types"
 import { ParsedUrlQuery } from "querystring"
 
@@ -28,22 +28,81 @@ function getComparisonForFilter(filterKey: ListingFilterKeys) {
   }
 }
 
-export function encodeToBackendFilterString(filterParams: ListingFilterParams) {
+export enum CommunityTypeOptionsEnum {
+  "all" = "all",
+  "seniorHousing" = "seniorHousing",
+  "specialNeedsAndDisability" = "specialNeedsAndDisability",
+}
+
+export interface ListingFilterState {
+  // todo avaleske use the values from filter keys for these field names
+  /**  */
+  neighborhood?: string
+
+  /**  */
+  bedrooms?: number
+
+  /**  */
+  zipcode?: string
+
+  /**  */
+  availability?: AvailabilityFilterEnum
+
+  /**  */
+  seniorHousing?: boolean
+
+  /** */
+  communityType?: CommunityTypeOptionsEnum
+
+  /**  */
+  minRent?: number
+
+  /**  */
+  maxRent?: number
+}
+
+// todo is this the best way to do this?
+const FrontendFilterKeys = { ...ListingFilterKeys, communityType: "communityType" }
+
+export function encodeToBackendFilterString(filterParams: ListingFilterState) {
   let queryString = ""
   for (const filterType in filterParams) {
-    if (filterType in ListingFilterKeys) {
+    if (filterType in ListingFilterKeys && filterType !== ListingFilterKeys.seniorHousing) {
       const comparison = getComparisonForFilter(ListingFilterKeys[filterType])
       queryString += `&filter[$comparison]=${comparison}&filter[${filterType}]=${filterParams[filterType]}`
+    }
+  }
+  // manually override the seniorHousing filter if the communityType was set
+  // todo this would be easier if we stored the query params as an object first.
+  // todo think more about the logic doing the override here. maybe we want to override differently
+  if (filterParams.communityType !== undefined || filterParams.seniorHousing !== undefined) {
+    if (filterParams.communityType === CommunityTypeOptionsEnum.seniorHousing) {
+      queryString += `&filter[$comparison]=NA&filter[seniorHousing]=true}`
+    } else if (filterParams.seniorHousing === true) {
+      queryString += `&filter[$comparison]=NA&filter[seniorHousing]=true}`
+    } else if (filterParams.seniorHousing === false) {
+      queryString += `&filter[$comparison]=NA&filter[seniorHousing]=false}`
     }
   }
   return queryString
 }
 
-export function encodeToFrontendFilterString(filterParams: ListingFilterParams) {
+// so the question then is when to make the transformation from drop down to booleans.
+// the frontend url needs to support booleans so the eligibility questionnaire can say "not senior housing"
+// but the modal doesn't support that. so we need a way to transform from the boolean to the modal.
+// but if the query string doesn't support the false option, we'll lose that data before we make the url request
+// there's a ux question here of why it needs to be a dropdown, but let's go with it for now.
+// seniorHousing = true -> store it that way -> add it to the url
+// communityType = true -> store it that way -> add the right communityType to the URL
+
+
+// so now these just pass through the url to the filter state, and do nothing to normalize it and ensure
+// the state makes sense re senior housing
+export function encodeToFrontendFilterString(filterParams: ListingFilterState) {
   let queryString = ""
   for (const filterType in filterParams) {
     const value = filterParams[filterType]
-    if (filterType in ListingFilterKeys && value !== undefined && value !== "") {
+    if (filterType in FrontendFilterKeys && value !== undefined && value !== "") {
       queryString += `&${filterType}=${value}`
     }
   }
@@ -51,10 +110,10 @@ export function encodeToFrontendFilterString(filterParams: ListingFilterParams) 
 }
 
 export function decodeFiltersFromFrontendUrl(query: ParsedUrlQuery) {
-  const filters: ListingFilterParams = {}
+  const filters: ListingFilterState = {}
   let foundFilterKey = false
   for (const queryKey in query) {
-    if (queryKey in ListingFilterKeys) {
+    if (queryKey in FrontendFilterKeys) {
       filters[queryKey] = query[queryKey]
       foundFilterKey = true
     }
