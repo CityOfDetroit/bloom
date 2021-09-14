c3bf9fff549f7f65f7d69368ace6c08d8f871428
elenm <16543000+elenm@users.noreply.github.com>
2021-09-14 15:36:39 -0400

    Remove unused availability filter method.

--- backend/core/src/shared/filter/custom_filters.ts
+++ backend/core/src/shared/filter/custom_filters.ts
@@ -25,19 +25,6 @@ export function addSeniorHousingQuery(qb: WhereExpression, filterValue: string) 
   }
 }
 
-function addAvailabilityParams(
-  qb: WhereExpression,
-  filterType: AvailabilityFilterEnum,
-  comparison: string,
-  filterValue: any
-) {
-  const hasAvailabilityColumnName = `LOWER(CAST(${filterTypeToFieldMap[filterType]} as text))`
-  const whereParameterName = filterType
-  qb.andWhere(`${hasAvailabilityColumnName} ${comparison} LOWER(:${whereParameterName})`, {
-    [whereParameterName]: filterValue,
-  })
-}
-
 export function addAvailabilityQuery(qb: WhereExpression, filterValue: AvailabilityFilterEnum) {
   const whereParameterName = "availability"
   switch (filterValue) {
