eaa37cb2cf593e3bea315fcd1c3059e15df62aa4
Austin Valeske <avaleske@google.com>
2021-09-20 17:12:55 -0400

    start of adding esri map. still broken

--- ui-components/src/page_components/listing/ListingMap.tsx
+++ ui-components/src/page_components/listing/ListingMap.tsx
@@ -1,6 +1,10 @@
-import React, { useState, useCallback, useEffect } from "react"
+import React, { useState, useCallback, useEffect, useRef } from "react"
 import "mapbox-gl/dist/mapbox-gl.css"
 import MapGL, { Marker } from "react-map-gl"
+// import { Map } from "@esri/react-arcgis"
+import { loadModules } from 'esri-loader';
+// import Map from "@arcgis/core/Map";
+// import MapView from "@arcgis/core/views/MapView";
 
 import "./ListingMap.scss"
 import { MultiLineAddress, Address } from "../../helpers/address"
@@ -35,6 +39,52 @@ const isValidLongitude = (longitude: number) => {
 }
 
 const ListingMap = (props: ListingMapProps) => {
+  const [renderMap, setRenderMap] = useState(false)
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    // setRenderMap(false)
+    if (mapDiv.current) {
+      loadModules(['esri/views/MapView', 'esri/Map', 'esri/Graphic']).then(([MapView, Map, Graphic]) => {
+        const map = new Map({
+          basemap: "streets-vector",
+        })
+        const mapView = new MapView({
+          map: map,
+          container: mapDiv.current,
+          center: [props.address?.longitude, props.address?.latitude],
+          zoom: 13
+        })
+
+        // create point
+        const point = {
+          type: "point", // autocasts as new Point()
+          longitude: props.address?.longitude,
+          latitude: props.address?.latitude
+        };
+
+        // Create a symbol for drawing the point
+        const markerSymbol = {
+          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+          color: [24, 37, 42],
+          outline: {
+            // autocasts as new SimpleLineSymbol()
+            color: [255, 255, 255],
+            width: 2
+          }
+        };
+
+        // Create a graphic and add the geometry and symbol to it
+        const pointGraphic = new Graphic({
+          geometry: point,
+          symbol: markerSymbol
+        });
+
+        mapView.graphics.add(pointGraphic)
+      })
+    }
+  }, [props.address?.latitude, props.address?.longitude])
+
   const [marker, setMarker] = useState({
     latitude: props.address?.latitude,
     longitude: props.address?.longitude,
@@ -100,7 +150,10 @@ const ListingMap = (props: ListingMapProps) => {
         {props.listingName && <h3 className="text-caps-tiny">{props.listingName}</h3>}
         <MultiLineAddress address={props.address} />
       </div>
-      {(process.env.mapBoxToken || process.env.MAPBOX_TOKEN) && (
+      <div style={{height: "400px"}} ref={mapDiv}></div>
+      {/* renderMap: {renderMap.toString()} */}
+      {/* {renderMap && <} */}
+      {/* {(process.env.mapBoxToken || process.env.MAPBOX_TOKEN) && (
         <MapGL
           mapboxApiAccessToken={process.env.mapBoxToken || process.env.MAPBOX_TOKEN}
           mapStyle="mapbox://styles/mapbox/streets-v11"
@@ -131,7 +184,7 @@ const ListingMap = (props: ListingMapProps) => {
               </>
             )}
         </MapGL>
-      )}
+      )} */}
     </div>
   )
 }
