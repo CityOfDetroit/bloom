36c1fdcb5e00e71553412cc5bf6f6aee17a9de84
elenm <16543000+elenm@users.noreply.github.com>
2021-09-22 16:07:10 -0400

    Fix filters test.

--- ui-components/__tests__/helpers/filters.test.ts
+++ ui-components/__tests__/helpers/filters.test.ts
@@ -1,26 +1,16 @@
-import { cleanup } from "@testing-library/react"
-import {
-  encodeToBackendFilterArray,
-  encodeToFrontendFilterString,
-  decodeFiltersFromFrontendUrl,
-} from "../../src/helpers/filters"
-import { parse } from "querystring"
-import {
-  EnumListingFilterParamsComparison,
-  EnumListingFilterParamsStatus,
-  ListingFilterParams,
-} from "@bloom-housing/backend-core/types"
+import {cleanup} from "@testing-library/react"
+import {FrontendFilterKey, FrontendFilterState,} from "../../src/helpers/filters"
+import {parse} from "querystring"
+import {EnumListingFilterParamsComparison, EnumListingFilterParamsStatus, ListingFilterParams,} from "@bloom-housing/backend-core/types"
 
 afterEach(cleanup)
 
 describe("encode backend filter array", () => {
   it("should handle single filter", () => {
-    const filter: ListingFilterParams = {
-      status: EnumListingFilterParamsStatus.active,
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(encodeToBackendFilterArray(filter)).toEqual([
+    const filterState = new FrontendFilterState()
+    filterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+
+    expect(filterState.getBackendFilterArray()).toEqual([
       {
         $comparison: EnumListingFilterParamsComparison["="],
         status: EnumListingFilterParamsStatus.active,
@@ -28,13 +18,11 @@ describe("encode backend filter array", () => {
     ])
   })
   it("should handle multiple filters", () => {
-    const filter: ListingFilterParams = {
-      name: "Name",
-      status: EnumListingFilterParamsStatus.active,
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(encodeToBackendFilterArray(filter)).toEqual([
+    const filterState = new FrontendFilterState()
+    filterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+    filterState.setValue(FrontendFilterKey.name, "Name")
+
+    expect(filterState.getBackendFilterArray()).toEqual([
       {
         $comparison: EnumListingFilterParamsComparison["="],
         name: "Name",
@@ -49,69 +37,66 @@ describe("encode backend filter array", () => {
 
 describe("encode frontend filter string", () => {
   it("should handle single filter", () => {
-    const filter: ListingFilterParams = {
-      status: EnumListingFilterParamsStatus.active,
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(encodeToFrontendFilterString(filter)).toBe("&status=active")
+    const filterState = new FrontendFilterState()
+    filterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+
+    expect(filterState.getFrontendFilterString()).toBe("&status=active")
   })
   it("should handle multiple filters", () => {
-    const filter: ListingFilterParams = {
-      name: "Name",
-      status: EnumListingFilterParamsStatus.active,
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(encodeToFrontendFilterString(filter)).toBe("&name=Name&status=active")
+    const filterState = new FrontendFilterState()
+    filterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+    filterState.setValue(FrontendFilterKey.name, "Name")
+
+    expect(filterState.getFrontendFilterString()).toBe("&name=Name&status=active")
   })
   it("should exclude empty filters", () => {
-    const filter: ListingFilterParams = {
-      name: "Name",
-      status: undefined,
-      zipcode: "",
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(encodeToFrontendFilterString(filter)).toBe("&name=Name")
+    const filterState = new FrontendFilterState()
+    filterState.setValue(FrontendFilterKey.status, undefined)
+    filterState.setValue(FrontendFilterKey.name, "Name")
+    filterState.setValue(FrontendFilterKey.zipcode, "")
+
+    expect(filterState.getFrontendFilterString()).toBe("&name=Name")
   })
 })
 
 describe("get filter from parsed url", () => {
   it("should handle single filter", () => {
     const filterString = parse("localhost:3000/listings?page=1&status=active")
-    const expected: ListingFilterParams = {
-      status: EnumListingFilterParamsStatus.active,
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(decodeFiltersFromFrontendUrl(filterString)).toStrictEqual(expected)
+    const actualFilterState = new FrontendFilterState()
+    const expectedFilterState = new FrontendFilterState()
+    expectedFilterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+
+    expect(actualFilterState.getFiltersFromFrontendUrl(filterString)).toStrictEqual(expectedFilterState)
   })
   it("should handle multiple filters", () => {
     const filterString = parse("localhost:3000/listings?page=1&status=active&name=Name")
-    const expected: ListingFilterParams = {
-      status: EnumListingFilterParamsStatus.active,
-      name: "Name",
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(decodeFiltersFromFrontendUrl(filterString)).toStrictEqual(expected)
+    const actualFilterState = new FrontendFilterState()
+    const expectedFilterState = new FrontendFilterState()
+    expectedFilterState.setValue(FrontendFilterKey.status, EnumListingFilterParamsStatus.active)
+    expectedFilterState.setValue(FrontendFilterKey.name, "Name")
+
+    expect(actualFilterState.getFiltersFromFrontendUrl(filterString)).toStrictEqual(expectedFilterState)
   })
   it("should handle no filters", () => {
     const filterString = parse("localhost:3000/listings?page=1")
-    expect(decodeFiltersFromFrontendUrl(filterString)).toBe(undefined)
+    const actualFilterState = new FrontendFilterState()
+    const expectedFilterState = new FrontendFilterState()
+
+    expect(actualFilterState.getFiltersFromFrontendUrl(filterString)).toStrictEqual(expectedFilterState)
   })
   it("should handle no known filter keys", () => {
     const filterString = parse("localhost:3000/listings?page=1&unknown=blah")
-    expect(decodeFiltersFromFrontendUrl(filterString)).toBe(undefined)
+    const actualFilterState = new FrontendFilterState()
+    const expectedFilterState = new FrontendFilterState()
+
+    expect(actualFilterState.getFiltersFromFrontendUrl(filterString)).toStrictEqual(expectedFilterState)
   })
   it("should handle some known filters", () => {
     const filterString = parse("localhost:3000/listings?page=1&unknown=blah&name=Name")
-    const expected: ListingFilterParams = {
-      name: "Name",
-      // $comparison is a required field even though it won't be used on the frontend. Will be fixed in #484.
-      $comparison: EnumListingFilterParamsComparison.NA,
-    }
-    expect(decodeFiltersFromFrontendUrl(filterString)).toStrictEqual(expected)
+    const actualFilterState = new FrontendFilterState()
+    const expectedFilterState = new FrontendFilterState()
+    expectedFilterState.setValue(FrontendFilterKey.name, "Name")
+
+    expect(actualFilterState.getFiltersFromFrontendUrl(filterString)).toStrictEqual(expectedFilterState)
   })
 })
