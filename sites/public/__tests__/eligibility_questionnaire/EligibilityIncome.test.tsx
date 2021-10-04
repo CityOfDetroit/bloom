c5760d114145960fb2ad9a036e92199e25fef4a0
elenm <16543000+elenm@users.noreply.github.com>
2021-10-04 18:41:19 -0400

    Mock up useAmiChartList.

--- sites/public/__tests__/eligibility_questionnaire/EligibilityIncome.test.tsx
+++ sites/public/__tests__/eligibility_questionnaire/EligibilityIncome.test.tsx
@@ -1,8 +1,9 @@
-import {render, fireEvent, screen, waitFor} from "@testing-library/react"
+import {render, fireEvent, screen } from "@testing-library/react"
 import EligibilityIncome from "../../pages/eligibility/income"
 import React from "react"
 import { act } from "react-dom/test-utils"
 import userEvent from "@testing-library/user-event"
+import {AmiChartCreateDto } from '@bloom-housing/backend-core/dist/src/ami-charts/dto/ami-chart.dto';
 
 const mockRouter = {
   push: jest.fn(),
@@ -13,11 +14,37 @@ jest.mock("next/router", () => ({
   },
 }))
 
+jest.mock("../../lib/hooks", () => {
+  const originalModule = jest.requireActual("../../lib/hooks");
+  const amiChart: AmiChartCreateDto = {
+    name: "Test AMI chart",
+    items: [
+      {
+        percentOfAmi: 20,
+        householdSize: 1,
+        income: 10000,
+      },
+      {
+        percentOfAmi: 30,
+        householdSize: 1,
+        income: 15000
+      } ]
+  }
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    useAmiChartList: jest.fn(()=> ({data: amiChart}))
+  };
+})
+
 describe("<EligibilityIncome>", () => {
+
   it("Renders Income page of eligibility questionnaire", () => {
     act(() => {
       render(<EligibilityIncome />)
     })
+
     expect(
       screen.getByRole("heading", { name: "What is your total household annual income?" })
     ).toBeInTheDocument()
@@ -25,10 +52,12 @@ describe("<EligibilityIncome>", () => {
   })
 
   it("Clicks the Done button", async () => {
+    debugger;
+
     await act(async () => {
       render(<EligibilityIncome />)
 
-      userEvent.type(screen.getByRole("spinbutton", { name: "Income" }), "10000")
+      userEvent.type(screen.getByRole("spinbutton", { name: "Income" }), "2")
       fireEvent.click(screen.getByRole("button", { name: "Done" }))
     })
 
