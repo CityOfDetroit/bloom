3181170ac11e88642e3cb6febc7e911b046c9735
Anders Schneider <anderseschneider@gmail.com>
2021-11-11 13:32:31 -0500

    Replace the logging with a TODO

--- backend/core/src/cron/cron.service.ts
+++ backend/core/src/cron/cron.service.ts
@@ -5,9 +5,6 @@ import { Cron, CronExpression } from "@nestjs/schedule"
 export class CronService {
   @Cron(CronExpression.EVERY_WEEK)
   handleCron() {
-    console.log(
-      "This is a log message from the CronService. It's an example of a task that can be performed " +
-        "on a cron schedule."
-    )
+    // TODO: add a cron task here.
   }
 }
