f4478be235d58f27be252a12689ed9ee9ce46b86
Anders Schneider <anderseschneider@gmail.com>
2021-12-22 18:07:56 -0500

    Refactor sendListingNotifications

--- backend/core/src/listings/listings-notifications.ts
+++ backend/core/src/listings/listings-notifications.ts
@@ -22,18 +22,11 @@ export class ListingsNotificationsConsumer {
   @Process()
   async sendListingNotifications(job: Job<ListingNotificationInfo>): Promise<StatusDto> {
     const listing: Listing = job.data.listing
-    let status: StatusDto
-    if (job.data.updateType === ListingUpdateType.CREATE) {
-      status = await this.smsService.sendNewListingNotification(listing)
+    const status: StatusDto = await this.smsService.sendNewListingNotification(listing)
 
-      // TODO(https://github.com/CityOfDetroit/bloom/issues/698): call out to the
-      // emailService to send email notifications.
-    } else if (job.data.updateType === ListingUpdateType.MODIFY) {
-      status = await this.smsService.sendNewListingNotification(listing)
+    // TODO(https://github.com/CityOfDetroit/bloom/issues/698): call out to the
+    // emailService to send email notifications.
 
-      // TODO(https://github.com/CityOfDetroit/bloom/issues/698): call out to the
-      // emailService to send email notifications.
-    }
     return status
   }
 }
