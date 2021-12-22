6d35567f066939f302c54e2fb3b70f0624bd95c0
Lint Action <lint-action@samuelmeuli.com>
2021-12-22 23:35:10 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1640192742859-favorites2.ts
+++ backend/core/src/migration/1640192742859-favorites2.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class favorites21640192742859 implements MigrationInterface {
-    name = 'favorites21640192742859'
+  name = "favorites21640192742859"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_preferences" ADD "favorite_i_ds" jsonb NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_preferences" ADD "favorite_i_ds" jsonb NOT NULL`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_preferences" DROP COLUMN "favorite_i_ds"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_preferences" DROP COLUMN "favorite_i_ds"`)
+  }
 }
