7f98c42dd5414245d3c928902c0274e33003d440
pfeeny5 <82653098+pfeeny5@users.noreply.github.com>
2021-12-30 11:04:33 -0500

    updated to include preference-entity default

---
+++ backend/core/src/migration/1640879376543-favoriteID2.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class favoriteID21640879376543 implements MigrationInterface {
+    name = 'favoriteID21640879376543'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_preferences" DROP COLUMN "favorite_i_ds"`);
+        await queryRunner.query(`ALTER TABLE "user_preferences" ADD "favorite_i_ds" jsonb NOT NULL DEFAULT '[]'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user_preferences" DROP COLUMN "favorite_i_ds"`);
+        await queryRunner.query(`ALTER TABLE "user_preferences" ADD "favorite_i_ds" json NOT NULL`);
+    }
+
+}
