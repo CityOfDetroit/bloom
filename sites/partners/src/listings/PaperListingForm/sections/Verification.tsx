2dfa00f93e6f51c46fd2163a8039f93696633ce8
Sean Albert <smabert@gmail.com>
2022-03-09 17:09:24 -0700

    feat: verify alert and scrollTo

--- sites/partners/src/listings/PaperListingForm/sections/Verification.tsx
+++ sites/partners/src/listings/PaperListingForm/sections/Verification.tsx
@@ -2,27 +2,36 @@ import React from "react"
 import { useFormContext } from "react-hook-form"
 import { t, GridSection, Field } from "@bloom-housing/ui-components"
 
-const Verification = () => {
+const Verification = ({
+  setAlert,
+}: {
+  setAlert: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
   const formMethods = useFormContext()
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { register } = formMethods
 
   return (
-    <div>
+    <div id="isVerifiedContainer">
       <GridSection
         grid={false}
         separator
         title={t("listings.sections.verification")}
         description={t("listings.sections.verificationSubtitle")}
       >
-        <GridSection columns={1}>
+        <GridSection columns={2}>
           <Field
-            label={t("listings.isVerified")}
+            label={t("listings.sections.isVerified")}
             name={"isVerified"}
             id={"isVerified"}
             type={"checkbox"}
             register={register}
+            inputProps={{
+              onChange: (e) => {
+                setAlert(!e.target.checked)
+              },
+            }}
           />
         </GridSection>
       </GridSection>
