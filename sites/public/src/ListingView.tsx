b4ed38dcc3f5afb7402c17ec00968cfe46590f19
Emily Jablonski <emily.jablonski@exygy.com>
2022-03-10 15:40:10 -0700

    fix: hide completely

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -293,16 +293,14 @@ export const ListingView = (props: ListingProps) => {
               data={[{ data: groupedUnitData }]}
               responsiveCollapse={true}
             />
-            <div className="text-sm leading-5 mt-4 invisible md:visible">
-              {t("listings.unitSummaryGroupMessage")}{" "}
-              {hmiData ? (
+            {hmiData.length > 0 && (
+              <div className="text-sm leading-5 mt-4 invisible md:visible">
+                {t("listings.unitSummaryGroupMessage")}{" "}
                 <a className="underline" href="#household_maximum_income_summary">
                   {t("listings.unitSummaryGroupLinkText")}
                 </a>
-              ) : (
-                t("listings.unitSummaryGroupLinkText")
-              )}
-            </div>
+              </div>
+            )}
           </>
         )}
       </div>
