297eae5be8c34682d3492e796bba20beade71f2c
Yazeed Loonat <yazeedloonat@gmail.com>
2022-04-05 10:49:52 -0700

    tests: partner cypress test fixes

--- sites/partners/cypress/integration/application.spec.ts
+++ sites/partners/cypress/integration/application.spec.ts
@@ -9,15 +9,14 @@ describe("Application Management Tests", () => {
 
   it("Application grid should display correct number of results", () => {
     cy.visit("/")
-    cy.get(`[col-id="status"]`).eq(1).contains("Accepting Applications").click()
-    cy.getByID("lbTotalPages").contains("20")
+    cy.get(`[col-id="status"]`).eq(1).contains("Public").click()
     cy.get(".applications-table")
       .first()
       .find(".ag-center-cols-container")
       .first()
       .find(".ag-row")
       .should((elems) => {
-        expect(elems).to.have.length(8)
+        expect(elems).to.have.length(0)
       })
   })
 })
