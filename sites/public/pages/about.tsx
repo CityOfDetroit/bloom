4f456c15d940abaa1d87ffe0630a02eb8520c607
Adrien Young <adrien.elizabeth@gmail.com>
2022-04-08 17:12:11 -0500

    fix: add partners list and remove copy from non-english json files

--- sites/public/pages/about.tsx
+++ sites/public/pages/about.tsx
@@ -1,15 +1,17 @@
 import React from "react"
-import { t, PageHeader } from "@bloom-housing/ui-components"
+import { MarkdownSection, t, PageHeader, GridSection, GridCell } from "@bloom-housing/ui-components"
 import Layout from "../layouts/application"
 
 export default function About() {
   return (
     <Layout>
       <PageHeader title={t("pageTitle.about")} inverse />
-      <div className="max-w-5xl m-auto px-5 mt-0 mb-5">
+      <div className="max-w-5xl m-auto px-5">
         <p className="my-8">{t("about.body1")}</p>
         <p className="my-8">{t("about.body2")}</p>
         <p>{t("about.moreInfoContact")}</p>
+        <p className="my-8">{t("about.thankYouPartners")}</p>
+        <p className="my-8">{t("about.partnersList")}</p>
       </div>
     </Layout>
   )
