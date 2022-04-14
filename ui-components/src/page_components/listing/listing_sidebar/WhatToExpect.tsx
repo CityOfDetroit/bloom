25cc3c26906842ac0b6da4e2ffebd7c5826e412c
Emily Jablonski <emily.jablonski@exygy.com>
2022-04-13 19:06:15 -0600

    feat: default what to expect text and additional what to expect field

--- ui-components/src/page_components/listing/listing_sidebar/WhatToExpect.tsx
+++ ui-components/src/page_components/listing/listing_sidebar/WhatToExpect.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
-import { t } from "../../../helpers/translator"
 import { Listing } from "@bloom-housing/backend-core/types"
+import Markdown from "markdown-to-jsx"
+import { t } from "../../../helpers/translator"
+import { ExpandableContent } from "../../../actions/ExpandableContent"
 
 interface WhatToExpectProps {
   listing: Listing
@@ -8,13 +10,22 @@ interface WhatToExpectProps {
 
 const WhatToExpect = (props: WhatToExpectProps) => {
   const listing = props.listing
-
+  if (!listing.whatToExpect || listing.whatToExpect === "") return null
   return (
     <section className="aside-block">
       <h4 className="text-caps-underline">{t("whatToExpect.label")}</h4>
-      <p className="text-tiny text-gray-800">
-        {listing.whatToExpect ? listing.whatToExpect : t("whatToExpect.default")}
-      </p>
+      <div className="text-tiny text-gray-750">
+        <Markdown options={{ disableParsingRawHTML: false }}>{listing.whatToExpect ?? ""}</Markdown>
+        {listing.whatToExpectAdditionalText && (
+          <div className={"mt-2"}>
+            <ExpandableContent>
+              <Markdown options={{ disableParsingRawHTML: false }}>
+                {listing.whatToExpectAdditionalText}
+              </Markdown>
+            </ExpandableContent>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
