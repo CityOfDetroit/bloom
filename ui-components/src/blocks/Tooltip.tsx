42e2b12570802800c557b0fca3e0fc9e9a812c47
Dominik Barcikowski <dominik@airnauts.com>
2022-04-22 14:15:36 +0200

    feat: Create tooltip component
    Closes #1096

---
+++ ui-components/src/blocks/Tooltip.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import useKeyPress from "../helpers/useKeyPress"
+import "./Tooltip.scss"
+
+export interface TooltipProps {
+  className?: string
+  id: string
+  text: string
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ className, id, text, children }) => {
+  const [isHidden, setHidden] = useState(true)
+
+  const show = () => setHidden(false)
+  const hide = () => setHidden(true)
+
+  useKeyPress("Escape", () => hide())
+
+  return (
+    <div className={`tooltip ${className || ""}`}>
+      <div
+        className="tooltip__element"
+        role="tooltip"
+        id={id}
+        hidden={isHidden}
+        data-testid="tooltip-element"
+      >
+        {text}
+      </div>
+
+      <div
+        className="tooltip__children"
+        data-testid="tooltip-children"
+        onFocus={show}
+        onMouseEnter={show}
+        onBlur={hide}
+        onMouseLeave={hide}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export { Tooltip as default, Tooltip }
