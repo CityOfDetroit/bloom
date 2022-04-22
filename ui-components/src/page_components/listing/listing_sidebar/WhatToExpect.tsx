a5bbfbfbbdf73e812b5e3b8bb2788af50135f650
Emily Jablonski <emily.jablonski@exygy.com>
2022-04-22 16:16:30 -0600

    refactor: remove backend dependencies, make component easier to backport

--- ui-components/src/page_components/listing/listing_sidebar/WhatToExpect.tsx
+++ ui-components/src/page_components/listing/listing_sidebar/WhatToExpect.tsx
@@ -1,27 +1,24 @@
 import * as React from "react"
-import { Listing } from "@bloom-housing/backend-core/types"
 import Markdown from "markdown-to-jsx"
 import { t } from "../../../helpers/translator"
 import { ExpandableContent } from "../../../actions/ExpandableContent"
 
 interface WhatToExpectProps {
-  listing: Listing
+  content: string
+  expandableContent: string
 }
 
-const WhatToExpect = (props: WhatToExpectProps) => {
-  const listing = props.listing
-  if (!listing.whatToExpect || listing.whatToExpect === "") return null
+const WhatToExpect = ({ content, expandableContent }: WhatToExpectProps) => {
+  if (!content || content === "") return null
   return (
     <section className="aside-block">
       <h4 className="text-caps-underline">{t("whatToExpect.label")}</h4>
       <div className="text-tiny text-gray-750">
-        <Markdown options={{ disableParsingRawHTML: false }}>{listing.whatToExpect ?? ""}</Markdown>
-        {listing.whatToExpectAdditionalText && (
+        <Markdown options={{ disableParsingRawHTML: false }}>{content}</Markdown>
+        {expandableContent && (
           <div className={"mt-2"}>
             <ExpandableContent>
-              <Markdown options={{ disableParsingRawHTML: false }}>
-                {listing.whatToExpectAdditionalText}
-              </Markdown>
+              <Markdown options={{ disableParsingRawHTML: false }}>{expandableContent}</Markdown>
             </ExpandableContent>
           </div>
         )}
