37f0327e4956c0d1a066c01ecd790fc4350e6ca9
Adrien Young <adrien.elizabeth@gmail.com>
2022-05-10 15:40:11 -0500

    feat: ListingDividerLine

---
+++ ui-components/src/page_components/listing/ListingDividerLine.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const ListingDividerLine = () => {
+  return <section className="w-full border-b border-gray-400 h-0"></section>
+}
+
+export { ListingDividerLine as default, ListingDividerLine }
