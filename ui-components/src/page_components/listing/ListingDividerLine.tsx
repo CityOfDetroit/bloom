0986aecce8acf2ebca8fad80b9b9af8847ccb372
Adrien Young <adrien.elizabeth@gmail.com>
2022-05-17 09:35:28 -0500

    style: make tooltip taller but overlap more so it displays the same but no line shows

--- ui-components/src/page_components/listing/ListingDividerLine.tsx
+++ ui-components/src/page_components/listing/ListingDividerLine.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
+import React from "react"
 
 const ListingDividerLine = () => {
-  return <section className="w-full border-b border-gray-400 h-0"></section>
+  return <section className="w-full border-b border-gray-400 h-0" />
 }
 
 export { ListingDividerLine as default, ListingDividerLine }
