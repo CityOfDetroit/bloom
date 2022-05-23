bbe80c4e1d9d72fa0abf045dc49c2c1f1d3713c6
Emily Jablonski <emily.jablonski@exygy.com>
2022-05-23 13:37:57 -0600

    fix: remove unused tests

--- sites/public/cypress/integration/listing-detail.spec.ts
+++ sites/public/cypress/integration/listing-detail.spec.ts
@@ -2,11 +2,6 @@ describe("Individual listing page", () => {
   it("clicks into an individual listing page", () => {
     cy.visit("/listings")
     cy.contains("See Details").click()
-
-    // Verify that the listing page has some data/headings that we expect it to have.
-    // NOTE: this assumes that the first listing in the group contains these features,
-    // which may or may not hold true as new seed listings are added.
-    // TODO: pick a particular listing to click on with known features.
     cy.contains("Listing Updated")
     cy.contains("Features")
     cy.contains("Neighborhood")
--- sites/public/cypress/integration/pages/my-applications.spec.ts
+++
@@ -1,9 +0,0 @@
-describe.skip("My applications page", function () {
-  it("renders the my applications page", function () {
-    cy.visit("/sign-in")
-    cy.signIn()
-    cy.url().should("include", "/account/dashboard")
-    cy.getByTestId("account-dashboard-applications").click()
-    cy.location("pathname").should("include", "/account/applications")
-  })
-})
