fdd35fdd3e7e990e3b3aa104c46c0eede1d695c8
Emily Jablonski <emily.jablonski@exygy.com>
2022-06-03 14:14:46 -0600

    fix: install dependencies

--- backend/core/src/seeder/seeds/jurisdictions.ts
+++ backend/core/src/seeder/seeds/jurisdictions.ts
@@ -11,7 +11,6 @@ export const defaultJurisdictions: JurisdictionCreateDto[] = [
     programs: [],
     publicUrl: "",
     emailFromAddress: "Detroit Housing",
-    rentalAssistanceDefault: "",
   },
 ]
 
