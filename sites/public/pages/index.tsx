2cc0ee98ac7795ce20d4de75dbcbf5b56924f677
ColinBuyck <colin.buyck@gmail.com>
2022-07-11 15:07:15 -0700

    fix: unused code and improved padding

--- sites/public/pages/index.tsx
+++ sites/public/pages/index.tsx
@@ -74,7 +74,6 @@ export default function Home({ latestListings, comingSoonListings }) {
       href={`/listings/filtered?page=1${encodeToFrontendFilterString({
         region: props.region[1],
       })}`}
-      // style={{ backgroundImage: `url(${regionImageUrls.get(props.region[1])})` }}
     >
       <img src={`${regionImageUrls.get(props.region[1])}`} />
       <p className={styles.region__text}>{props.region[1]}</p>
