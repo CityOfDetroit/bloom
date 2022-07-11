ca9499a67938c00df57404a7be3349d3b807fba9
Adrien Young <adrien.elizabeth@gmail.com>
2022-07-11 15:53:34 -0500

    feat: ResourceLinkCard

---
+++ sites/public/src/ResourceLinkCard.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { MarkdownSection, Icon } from "@bloom-housing/ui-components"
+import { IconTypes } from "../../../ui-components/src/icons/Icon"
+
+interface ResourceLinkCardProps {
+  iconSymbol: IconTypes
+  title: string
+  subtitle: string
+  linkUrl: string
+  linkLabel: string
+}
+
+const ResourceLinkCard = (props: ResourceLinkCardProps) => {
+  const { iconSymbol, title, subtitle, linkUrl, linkLabel } = props
+
+  return (
+    <div className="border-b">
+      <MarkdownSection>
+        <Icon
+          fill="black"
+          size="large"
+          symbol={iconSymbol}
+          className="ml-2 px-2 info-cards__title"
+        />
+        <h3 className="font-semibold mt-0">{title}</h3>
+        <div className="mb-4">{subtitle}</div>
+        <a className="underline" href={linkUrl}>
+          {linkLabel}
+        </a>
+      </MarkdownSection>
+    </div>
+  )
+}
+
+export default ResourceLinkCard
