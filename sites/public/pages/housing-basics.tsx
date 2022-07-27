f9be41f38f6f92613ce8a3d73f78b085ef0be6e6
ColinBuyck <colin.buyck@gmail.com>
2022-07-27 12:25:44 -0700

    fix: removed unused styling

--- sites/public/pages/housing-basics.tsx
+++ sites/public/pages/housing-basics.tsx
@@ -36,7 +36,7 @@ export default function HousingBasics() {
         open={openModal}
         onClose={() => setOpenModal(false)}
         title={currentVideoTitle}
-        modalClassNames={"pb-0 px-0 border-none max-w-3xl"}
+        modalClassNames={"border-none max-w-3xl"}
         innerClassNames={"p-0"}
       >
         <Video label={currentVideoTitle} videoId={currentVideoID} ccLang={language} />
