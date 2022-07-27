6233175e147c2c5784ae9c7387236adf535968d7
Sean Albert <smabert@gmail.com>
2022-07-26 19:17:56 -0700

    fix: merge issues

--- ui-components/__tests__/page_components/SubmitApplication.test.tsx
+++ ui-components/__tests__/page_components/SubmitApplication.test.tsx
@@ -16,32 +16,4 @@ describe("<SubmitApplication>", () => {
     expect(getByText("Drop Off Address Street", { exact: false })).toBeTruthy()
     expect(getByText("Office Hours Header")).toBeTruthy()
   })
-  it("includes mailing address, excludes dropoff address, includes postmarks, excludes due date", () => {
-    const { getByText, queryByText } = render(<MailingYesPostmarksNoDueDate />)
-    expect(getByText("Submit a Paper Application")).toBeTruthy()
-    expect(getByText("Send Application by US Mail")).toBeTruthy()
-    expect(getByText("Mailing Address Street", { exact: false })).toBeTruthy()
-    expect(
-      getByText(
-        "Applications must be received by the deadline. If sending by U.S. Mail, the application must be received by mail no later than November 30th, 2021. Applications received after November 30th, 2021 via mail will not be accepted. Developer is not responsible for lost or delayed mail.",
-        { exact: false }
-      )
-    ).toBeTruthy()
-    expect(queryByText("or")).toBe(null)
-    expect(queryByText("Drop Off Application")).toBe(null)
-  })
-  it("includes mailing address, excludes dropoff address, excludes postmarks, includes due date", () => {
-    const { getByText, queryByText } = render(<MailingNoPostmarksYesDueDate />)
-    expect(getByText("Submit a Paper Application")).toBeTruthy()
-    expect(getByText("Send Application by US Mail")).toBeTruthy()
-    expect(getByText("Mailing Address Street", { exact: false })).toBeTruthy()
-    expect(
-      getByText(
-        "Applications must be received by the deadline. If sending by U.S. Mail, the application must be postmarked by November 29th, 2021. Developer is not responsible for lost or delayed mail.",
-        { exact: false }
-      )
-    ).toBeTruthy()
-    expect(queryByText("or")).toBe(null)
-    expect(queryByText("Drop Off Application")).toBe(null)
-  })
 })
