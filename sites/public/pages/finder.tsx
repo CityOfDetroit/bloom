d5bdc3fe483999eed6c3c1660ae6764d432575eb
ColinBuyck <colin.buyck@gmail.com>
2022-09-12 12:04:44 -0700

    fix: basic components

--- sites/public/pages/finder.tsx
+++ sites/public/pages/finder.tsx
@@ -1,17 +1,33 @@
-import { FormCard, PageHeader, t } from "@bloom-housing/ui-components"
+import { FormCard, PageHeader, ProgressNav, StepHeader, t } from "@bloom-housing/ui-components"
 import React from "react"
 import Layout from "../layouts/application"
 
 export default function Finder() {
+  const hm = () => {
+    return (
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <div className="text-lg font-bold">Find Listings For You</div>
+          <StepHeader
+            currentStep={1}
+            totalSteps={3}
+            stepPreposition={"of"}
+            stepLabeling={["steps"]}
+          ></StepHeader>
+        </div>
+        <ProgressNav
+          currentPageSection={1}
+          completedSections={1}
+          labels={["You", "Household", "Income", "Preferences", "Review"]}
+          mounted={true}
+          style="bar"
+        ></ProgressNav>
+      </div>
+    )
+  }
   return (
     <Layout>
-      <PageHeader
-        title={t("pageTitle.housingBasics")}
-        subtitle={t("pageDescription.housingBasics")}
-        // inverse
-      >
-        <FormCard children={""}></FormCard>
-      </PageHeader>
+      <PageHeader>{hm()}</PageHeader>
     </Layout>
   )
 }
