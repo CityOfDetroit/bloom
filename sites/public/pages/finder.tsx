b8710b331b0e3590bca62b270e5ebb480eb98869
ColinBuyck <colin.buyck@gmail.com>
2022-09-20 16:52:30 -0700

    fix: object pattern for multiple questions

--- sites/public/pages/finder.tsx
+++ sites/public/pages/finder.tsx
@@ -19,11 +19,6 @@ import router from "next/router"
 import React, { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
 import Layout from "../layouts/application"
-interface optionInterface {
-  value: string
-  label: string
-  translation?: string
-}
 
 const getTranslationString = (str: string) => {
   if (str === "studio") {
@@ -39,26 +34,45 @@ const getTranslationString = (str: string) => {
   }
 }
 
+interface finderField {
+  label: string
+  translation: string
+  selected: boolean
+}
+
+interface finderQuestion {
+  fieldGroupName: string
+  fields: finderField[]
+}
+
+interface finderForm {
+  [key: number]: finderQuestion
+}
+
 const Finder = () => {
-  const [bedroomOptions, setBedroomOptions] = useState<optionInterface[]>([])
+  const [currentStep, setCurrentStep] = useState<number>(1)
+  const [completedSteps, setCompletedSteps] = useState<number>(0)
+  const [activeQuestion, setActiveQuestion] = useState<finderQuestion>(null)
+  const formData: finderForm = {}
+
   const onSubmit = (data: ListingFilterState) => {
-    console.log(data)
     void router.push(`/listings/filtered?page=${1}&limit=${8}${encodeToFrontendFilterString(data)}`)
   }
   useEffect(() => {
     const getAndSetOptions = async () => {
       try {
         const response = await axios.get(`${process.env.backendApiBase}/listings/meta`)
+        // console.log(response.data)
         if (response.data) {
           if (response.data.unitTypes) {
-            setBedroomOptions(
-              response.data.unitTypes.map((elem) => ({
-                label: elem.name,
-                value: elem.id,
-                translation: getTranslationString(elem.name),
-              }))
-            )
+            const bedroomFields = response.data.unitTypes.map((elem) => ({
+              label: elem.name,
+              translation: getTranslationString(elem.name),
+              selected: false,
+            }))
+            formData[1] = { fieldGroupName: "bedRoomSize", fields: bedroomFields }
           }
+          setActiveQuestion(formData[1])
         }
       } catch (e) {
         console.error(e)
@@ -66,11 +80,10 @@ const Finder = () => {
     }
     void getAndSetOptions()
   }, [])
-
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, getValues } = useForm()
 
-  const ProgressHeader = () => {
+  const ProgressHeader = (currentStep: number, completedSteps: number) => {
     return (
       <div className="flex flex-col w-full">
         <div className="flex flex-row justify-between">
@@ -78,15 +91,15 @@ const Finder = () => {
             {t("listingFilters.buttonTitleExtended")}
           </div>
           <StepHeader
-            currentStep={1}
+            currentStep={currentStep}
             totalSteps={3}
             stepPreposition={"of"}
             stepLabeling={["steps"]}
           ></StepHeader>
         </div>
         <ProgressNav
-          currentPageSection={1}
-          completedSections={1}
+          currentPageSection={currentStep}
+          completedSections={completedSteps}
           labels={["Housing Needs", "Accessibility", "Building Types"]}
           mounted={true}
           style="bar"
@@ -95,52 +108,99 @@ const Finder = () => {
     )
   }
 
+  const nextQuestion = () => {
+    const userSelections = getValues()[formData[currentStep]["fieldGroupName"]]
+    formData[currentStep]["fields"].forEach(
+      (field) => (field["selected"] = userSelections.includes(field))
+    )
+    setCompletedSteps(completedSteps + 1)
+    setCurrentStep(currentStep + 1)
+  }
+  const previousQuestion = () => {
+    const userSelections = getValues()[formData[currentStep]["fieldGroupName"]]
+    formData[currentStep]["fields"].forEach(
+      (field) => (field["selected"] = userSelections.includes(field))
+    )
+    setCompletedSteps(completedSteps - 1)
+    setCurrentStep(currentStep - 1)
+  }
+
   return (
     <Layout>
       <Form onSubmit={handleSubmit(onSubmit)} className="bg-gray-300 border-t border-gray-450">
         <div className="md:mb-20 md:mt-12 mx-auto max-w-5xl">
-          {ProgressHeader()}
+          {ProgressHeader(currentStep, completedSteps)}
           <FormCard>
-            <div className="px-20">
-              <div className="pt-12">
-                <div className="text-3xl pb-4">{t("finder.bedroomQuestions")}</div>
-                <div className="pb-8 border-b border-gray-450">{t("finder.questionSubtitle")}</div>
-              </div>
-              <div className="py-8">
-                <p className="pb-4">{t("finder.multiselectHelper")}</p>
-                <div className="finder-grid">
-                  {bedroomOptions.map((elem) => {
-                    console.log(FrontendListingFilterStateKeys[elem.label])
-                    return (
-                      <div className="finder-grid__field">
-                        <Field
-                          name="bedRoomSize"
-                          register={register}
-                          id={FrontendListingFilterStateKeys[elem.label]}
-                          label={t(`listingFilters.bedroomsOptions.${elem.translation}`)}
-                          key={FrontendListingFilterStateKeys[elem.label]}
-                          type="checkbox"
-                          inputProps={{
-                            value: FrontendListingFilterStateKeys[elem.label],
-                          }}
-                          bordered
-                        />
-                      </div>
-                    )
-                  })}
+            {activeQuestion && (
+              <>
+                <div className="px-20">
+                  <div className="pt-12">
+                    <div className="text-3xl pb-4">
+                      {t(`finder.${activeQuestion["fieldGroupName"]}.question`)}
+                    </div>
+                    <div className="pb-8 border-b border-gray-450">
+                      {t("finder.questionSubtitle")}
+                    </div>
+                  </div>
+                  <div className="py-8">
+                    <p className="pb-4">{t("finder.multiselectHelper")}</p>
+                    <div className="finder-grid">
+                      {activeQuestion["fields"]?.map((field) => {
+                        return (
+                          <div className="finder-grid__field">
+                            <Field
+                              name="bedRoomSize"
+                              register={register}
+                              id={FrontendListingFilterStateKeys[field.label]}
+                              label={t(`listingFilters.bedroomsOptions.${field.translation}`)}
+                              key={FrontendListingFilterStateKeys[field.label]}
+                              type="checkbox"
+                              inputProps={{
+                                value: FrontendListingFilterStateKeys[field.label],
+                                defaultChecked: field.selected,
+                              }}
+                              bordered
+                            />
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="bg-gray-300 flex flex-row-reverse py-8 pr-20">
-              <Button type="submit" styleType={AppearanceStyleType.primary}>
-                {t("t.next")}
-              </Button>
-            </div>
-            <div className="flex justify-center align-center bg-white py-8">
-              <a className="underline" href="/listings">
-                {t("finder.skip")}
-              </a>
-            </div>
+
+                <div className="bg-gray-300 flex flex-row-reverse justify-between py-8 px-20">
+                  {/* change as more questions added */}
+                  {currentStep === 1 ? (
+                    <Button type="submit" styleType={AppearanceStyleType.primary}>
+                      {t("t.submit")}
+                    </Button>
+                  ) : (
+                    <Button
+                      type="button"
+                      onClick={() => nextQuestion()}
+                      styleType={AppearanceStyleType.primary}
+                    >
+                      {t("t.next")}
+                    </Button>
+                  )}
+                  {completedSteps > 0 && (
+                    <Button
+                      type="button"
+                      onClick={() => previousQuestion()}
+                      styleType={AppearanceStyleType.primary}
+                    >
+                      {t("t.back")}
+                    </Button>
+                  )}
+                </div>
+
+                <div className="flex justify-center align-center bg-white py-8">
+                  <a className="underline" href="/listings">
+                    {t("finder.skip")}
+                  </a>
+                </div>
+              </>
+            )}
           </FormCard>
         </div>
       </Form>
