43fd724791c23c0fbf1e4a65f858b502188d66b5
ColinBuyck <colin.buyck@gmail.com>
2022-10-01 00:51:36 -0700

    fix: remove unused  div

--- sites/public/pages/finder.tsx
+++ sites/public/pages/finder.tsx
@@ -162,17 +162,15 @@ const Finder = () => {
             {formData?.length > 0 && (
               <>
                 <div className="px-10 md:px-20 pt-6 md:pt-12 ">
-                  <div className="">
-                    <div className="text-3xl pb-4">
-                      {!isDisclaimer
-                        ? t(`finder.${activeQuestion?.fieldGroupName}.question`)
-                        : t("finder.disclaimer.header")}
-                    </div>
-                    <div className="pb-8 border-b border-gray-450">
-                      {!isDisclaimer
-                        ? t("finder.question.subtitle")
-                        : t("finder.disclaimer.subtitle")}
-                    </div>
+                  <div className="text-3xl pb-4">
+                    {!isDisclaimer
+                      ? t(`finder.${activeQuestion?.fieldGroupName}.question`)
+                      : t("finder.disclaimer.header")}
+                  </div>
+                  <div className="pb-8 border-b border-gray-450">
+                    {!isDisclaimer
+                      ? t("finder.question.subtitle")
+                      : t("finder.disclaimer.subtitle")}
                   </div>
                   {!isDisclaimer ? (
                     <div className="py-8">
