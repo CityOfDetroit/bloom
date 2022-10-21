a8116d12ca5a534f4477e18007682e894c654897
ColinBuyck <colin.buyck@gmail.com>
2022-10-21 09:17:59 -0700

    fix: disclaimer unique key

--- sites/public/src/forms/finder/FinderDisclaimer.tsx
+++ sites/public/src/forms/finder/FinderDisclaimer.tsx
@@ -7,7 +7,9 @@ const FinderDisclaimer = () => (
     </AlertBox>
     <ul className="list-disc list-inside py-8 flex flex-col gap-y-4">
       {[1, 2, 3, 4, 5].map((num) => (
-        <li className="pl-2 text-gray-700">{t(`finder.disclaimer.info${num}`)}</li>
+        <li key={`disclaimer_${num}`} className="pl-2 text-gray-700">
+          {t(`finder.disclaimer.info${num}`)}
+        </li>
       ))}
     </ul>
   </div>
