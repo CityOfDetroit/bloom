7a44731d57949a6ba7787976dcd7a5eb9f396b9f
ColinBuyck <colin.buyck@gmail.com>
2023-03-08 13:05:21 -0800

    fix: wip error test

--- sites/partners/__tests__/pages/listings/index.test.tsx
+++ sites/partners/__tests__/pages/listings/index.test.tsx
@@ -5,11 +5,12 @@ import {
 } from "@bloom-housing/shared-helpers"
 
 import { render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { rest } from "msw"
 import { setupServer } from "msw/node"
 import ListingsList from "../../../src/pages/index"
 import React from "react"
-import { listing } from "../../testHelpers"
+import { listing, user } from "../../testHelpers"
 
 const server = setupServer()
 
@@ -34,9 +35,7 @@ describe("listings", () => {
       rest.get("http://localhost:3100/listings", (_req, res, ctx) => {
         return res(ctx.json({ items: [listing], meta: { totalItems: 1, totalPages: 1 } }))
       }),
-      rest.get("http://localhost:3100/listings/csv", (_req, res, ctx) => {
-        return res(ctx.json({}))
-      }),
+
       rest.get("http://localhost:3100/user", (_req, res, ctx) => {
         return res(ctx.json({ id: "user1", roles: { id: "user1", isAdmin: true } }))
       })
@@ -49,6 +48,7 @@ describe("listings", () => {
         </AuthProvider>
       </ConfigProvider>
     )
+
     const exportButton = await findByText("Export to CSV")
     expect(exportButton).toBeInTheDocument()
   })
@@ -78,5 +78,38 @@ describe("listings", () => {
     )
     const exportButton = queryByText("Export to CSV")
     expect(exportButton).not.toBeInTheDocument()
+  })
+
+  it("should render Export to CSV pt 2 when user is Admin", async () => {
+    jest.useFakeTimers()
+    const fakeToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ZTMxODNhOC0yMGFiLTRiMDYtYTg4MC0xMmE5NjYwNmYwOWMiLCJpYXQiOjE2Nzc2MDAxNDIsImV4cCI6MjM5NzkwMDc0Mn0.ve1U5tAardpFjNyJ_b85QZLtu12MoMTa2aM25E8D1BQ"
+    window.sessionStorage.setItem(ACCESS_TOKEN_LOCAL_STORAGE_KEY, fakeToken)
+    server.use(
+      rest.get("http://localhost:3100/listings", (_req, res, ctx) => {
+        return res(ctx.json({ items: [listing], meta: { totalItems: 1, totalPages: 1 } }))
+      }),
+      rest.get("http://localhost:3100/listings/csv", (_req, res, ctx) => {
+        return res(ctx.json({}))
+      }),
+      rest.get("http://localhost:3100/user", (_req, res, ctx) => {
+        return res(ctx.json({ id: "user1", roles: { id: "user1", isAdmin: true } }))
+      })
+    )
+
+    const { findByText } = render(
+      <ConfigProvider apiUrl={"http://localhost:3100"}>
+        <AuthProvider>
+          <ListingsList />
+        </AuthProvider>
+      </ConfigProvider>
+    )
+    const exportButton = await findByText("Export to CSV")
+    expect(exportButton).toBeInTheDocument()
+    await userEvent.click(exportButton)
+    const error = await findByText(
+      "Export failed. Please try again later. If the problem persists, please email supportbloom@exygy.com"
+    )
+    expect(error).toBeInTheDocument()
   })
 })
