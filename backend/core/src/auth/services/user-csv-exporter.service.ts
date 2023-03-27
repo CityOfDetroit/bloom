9c1e42dd2029b4e7e3eb3ac81d988e63ddb1c6f7
ColinBuyck <colin.buyck@gmail.com>
2023-03-27 13:09:19 -0700

    fix: timezone temporary approach

--- backend/core/src/auth/services/user-csv-exporter.service.ts
+++ backend/core/src/auth/services/user-csv-exporter.service.ts
@@ -22,12 +22,12 @@ export class UserCsvExporterService {
         "Last Name": user.lastName,
         Email: user.email,
         Role: status.join(", "),
-        "Date Created": dayjs(user.createdAt).format("MM-DD-YYYY HH:mmZ[Z]"),
+        "Date Created": dayjs(user.createdAt).format("MM-DD-YYYY HH:mmA"),
         Status: user.confirmedAt ? "Confirmed" : "Unconfirmed",
         "Listing Names":
           user.leasingAgentInListings?.map((listing) => listing.name).join(", ") || "",
         "Listing Ids": user.leasingAgentInListings?.map((listing) => listing.id).join(", ") || "",
-        "Last Logged In": dayjs(user.lastLoginAt).format("MM-DD-YYYY HH:mmZ[Z]"),
+        "Last Logged In (UTC)": dayjs(user.lastLoginAt).format("MM-DD-YYYY HH:mmA"),
       }
       return obj
     }, {})
