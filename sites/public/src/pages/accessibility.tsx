4dd31de82fe7d9b8af587068dfa6e59f3cb29684
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2023-03-27 15:52:23 -0700

    fix: add a11y statement (#1601)
    * fix: added english a11y statement
    
    * fix: formatting feedback
    
    * fix: spanish added
    
    * fix: add ar and bn
    
    * fix: remove unused div and styling

---
+++ sites/public/src/pages/accessibility.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useContext } from "react"
+import { MarkdownSection, t } from "@bloom-housing/ui-components"
+import { PageHeader } from "../../../../detroit-ui-components/src/headers/PageHeader"
+import Markdown from "markdown-to-jsx"
+import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
+import { UserStatus } from "../lib/constants"
+import Layout from "../layouts/application"
+import pageContent from "../md_content/accessibility.md"
+import RenderIf from "../RenderIf"
+
+const Accessibility = () => {
+  const { profile } = useContext(AuthContext)
+
+  useEffect(() => {
+    pushGtmEvent<PageView>({
+      event: "pageView",
+      pageTitle: "Accessibility",
+      status: profile ? UserStatus.LoggedIn : UserStatus.NotLoggedIn,
+    })
+  }, [profile])
+
+  const pageTitle = <>{t("pageTitle.accessibilityStatement")}</>
+
+  return (
+    <Layout>
+      <PageHeader inverse={true} title={pageTitle} />
+      <MarkdownSection>
+        <Markdown
+          options={{
+            overrides: {
+              RenderIf,
+            },
+          }}
+        >
+          {pageContent}
+        </Markdown>
+      </MarkdownSection>
+    </Layout>
+  )
+}
+
+export default Accessibility
