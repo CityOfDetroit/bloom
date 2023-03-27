5a3b4c2ec6d4c930e6c6d14713e5373f836f64ea
ColinBuyck <colin.buyck@gmail.com>
2023-03-27 13:53:48 -0700

    fix: formatting case consistency

--- backend/core/src/auth/services/user-csv-exporter.service.ts
+++ backend/core/src/auth/services/user-csv-exporter.service.ts
@@ -22,12 +22,12 @@ export class UserCsvExporterService {
         "Last Name": user.lastName,
         Email: user.email,
         Role: status.join(", "),
-        "Date Created": dayjs(user.createdAt).format("MM-DD-YYYY HH:mmA"),
+        "Date Created": dayjs(user.createdAt).format("MM-DD-YYYY hh:mmA"),
         Status: user.confirmedAt ? "Confirmed" : "Unconfirmed",
         "Listing Names":
           user.leasingAgentInListings?.map((listing) => listing.name).join(", ") || "",
         "Listing Ids": user.leasingAgentInListings?.map((listing) => listing.id).join(", ") || "",
-        "Last Logged In (UTC)": dayjs(user.lastLoginAt).format("MM-DD-YYYY HH:mmA"),
+        "Last Logged In (UTC)": dayjs(user.lastLoginAt).format("MM-DD-YYYY hh:mmA"),
       }
       return obj
     }, {})
