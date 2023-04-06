11d17f74f33dfa6c08cfda11674d46b1771ce9c2
Morgan Ludtke <ludtkemorgan@gmail.com>
2023-04-06 17:32:15 -0500

    fix: update to latest ui-components

--- shared-helpers/src/AuthContext.ts
+++ shared-helpers/src/AuthContext.ts
@@ -180,7 +180,7 @@ const reducer = createReducer(
 )
 
 export const AuthContext = createContext<Partial<ContextProps>>({})
-export const AuthProvider: FunctionComponent = ({ children }) => {
+export const AuthProvider: FunctionComponent<React.PropsWithChildren> = ({ children }) => {
   const { apiUrl, storageType } = useContext(ConfigContext)
   const { router } = useContext(NavigationContext)
   const [state, dispatch] = useReducer(reducer, {
