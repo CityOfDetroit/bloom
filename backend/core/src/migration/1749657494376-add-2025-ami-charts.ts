2a3c2aac9329728d17127547cfc27d5c0423f463
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2025-06-25 11:27:11 -0500

    fix: add 2025 ami charts (#1676)
    * fix: add 2025 ami charts
    
    * fix: use the correct ami chart

---
+++ backend/core/src/migration/1749657494376-add-2025-ami-charts.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { amiHUD2025 } from "../../scripts/script-data/HUD2025";
+import { amiMSHDA2025 } from "../../scripts/script-data/MSHDA2025";
+
+export class homeAmiChart1749657494376 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const [{ id: juris }] = await queryRunner.query(
+      `SELECT id FROM jurisdictions WHERE name = 'Detroit'`
+    );
+    
+    await queryRunner.query(`
+                  INSERT INTO ami_chart
+                  (name, items, jurisdiction_id)
+                  VALUES ('${amiHUD2025.name}', '${JSON.stringify(amiHUD2025.items)}', '${juris}')
+              `);
+    await queryRunner.query(`
+                  INSERT INTO ami_chart
+                  (name, items, jurisdiction_id)
+                  VALUES ('${amiMSHDA2025.name}', '${JSON.stringify(amiMSHDA2025.items)}', '${juris}')
+              `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
