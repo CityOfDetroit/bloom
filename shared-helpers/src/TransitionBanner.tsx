559311f7eeaf6e412f353af4e992389124f5f053
ColinBuyck <colin.buyck@gmail.com>
2025-08-12 14:22:35 -0700

    fix: use boundToLayout prop

--- shared-helpers/src/TransitionBanner.tsx
+++ shared-helpers/src/TransitionBanner.tsx
@@ -4,7 +4,12 @@ import { AlertBox, t } from "@bloom-housing/ui-components"
 
 export const TransitionBanner = () => (
   <div className="site-banner-container">
-    <AlertBox type="alert" closeable={false} className="site-alert-banner-content">
+    <AlertBox
+      type="alert"
+      boundToLayoutWidth={true}
+      closeable={false}
+      className="site-alert-banner-content"
+    >
       <Markdown>{t("alert.unavailable")}</Markdown>
     </AlertBox>
   </div>
